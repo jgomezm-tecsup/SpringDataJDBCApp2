572a2385814beccde0a73c3b0771bcdffe4f18a8
Jaime <you@example.com>
2021-11-30 20:08:49 -0500

    Update

--- src/main/java/pe/edu/tecsup/springbootapp/repositories/CategoriaRepositoryImpl.java
+++ src/main/java/pe/edu/tecsup/springbootapp/repositories/CategoriaRepositoryImpl.java
@@ -2,6 +2,8 @@ package pe.edu.tecsup.springbootapp.repositories;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 
 import pe.edu.tecsup.springbootapp.entities.Categoria;
@@ -9,6 +11,9 @@ import pe.edu.tecsup.springbootapp.entities.Categoria;
 @Repository
 public class CategoriaRepositoryImpl implements CategoriaRepository {
 
+	private static Logger log = LoggerFactory.getLogger(CategoriaRepositoryImpl.class);
+	
+	
 	@Override
 	public List<Categoria> listar() throws Exception {
 		// TODO Auto-generated method stub
