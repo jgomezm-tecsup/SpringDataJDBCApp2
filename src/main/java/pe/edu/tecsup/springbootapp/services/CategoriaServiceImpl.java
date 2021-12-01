039debb6e415efbf9287a3744a42c7d340096bdb
Jaime <you@example.com>
2021-11-30 20:23:58 -0500

    Update

--- src/main/java/pe/edu/tecsup/springbootapp/services/CategoriaServiceImpl.java
+++ src/main/java/pe/edu/tecsup/springbootapp/services/CategoriaServiceImpl.java
@@ -4,20 +4,28 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import pe.edu.tecsup.springbootapp.entities.Categoria;
+import pe.edu.tecsup.springbootapp.repositories.CategoriaRepository;
 
 @Service
 public class CategoriaServiceImpl implements CategoriaService {
 
 	private static Logger log = LoggerFactory.getLogger(CategoriaServiceImpl.class);
 
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+
 	
 	@Override
 	public List<Categoria> listar() throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		
+		log.info("calling services ....!") ;
+		
+		return categoriaRepository.listar();
+		
 	}
 
 }
