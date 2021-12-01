65fa71af4ed5fb027815e71c8a5b5c6c60b6bcf9
Jaime <you@example.com>
2021-11-30 20:06:54 -0500

    Update

---
+++ src/main/java/pe/edu/tecsup/springbootapp/repositories/CategoriaRepositoryImpl.java
@@ -0,0 +1,18 @@
+package pe.edu.tecsup.springbootapp.repositories;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import pe.edu.tecsup.springbootapp.entities.Categoria;
+
+@Repository
+public class CategoriaRepositoryImpl implements CategoriaRepository {
+
+	@Override
+	public List<Categoria> listar() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
