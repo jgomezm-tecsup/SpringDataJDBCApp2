248283f40343bb0d129ed4a261a60e4a2464be5e
Jaime <you@example.com>
2021-11-30 20:40:41 -0500

    Update

---
+++ src/test/java/pe/edu/tecsup/springbootapp/services/CategoriaServiceTest.java
@@ -0,0 +1,20 @@
+package pe.edu.tecsup.springbootapp.services;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CategoriaServiceTest {
+
+	@Autowired
+	CategoriaService categoriaService;
+	
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
